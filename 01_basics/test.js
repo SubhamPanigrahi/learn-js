ea87302d149c1fdfc6206ae3ac5d0896157264c3
SubhamPanigrahi <subhamp.official@gmail.com>
2024-10-04 20:01:16 +0530

    some old notes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,22 @@
-console.log("Day 1 of learning JavaScript! It'll be a fun journey!")
+// function printHello(){
+//     for(let i = 0; i<100; i++){
+//         console.log("Hello")
+//     }
+// }
+
+// function printWorld(){
+//     for(let i = 0; i<100; i++){
+//         console.log("World")
+//     }
+// }
+
+// printHello();
+// printWorld();
+
+const fs = require("fs")
+
+fs.readFile("a.txt", "utf-8", function(err, data) {
+    console.log(data);
+});
+
+console.log("hi there from here!")
